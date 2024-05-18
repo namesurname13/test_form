a43e481c6dbc5c33b92eee2ad4211d7326d32a16
Ihor Belodedov <i.belodedov@rqc.ru>
2024-05-18 21:04:58 +0300

    parse string

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,3 +1,7 @@
+import { convertFromRaw } from "draft-js";
+import { stateToHTML } from "draft-js-export-html";
+import { FORMAT_OPTIONS } from "./constants";
+
 // Функция для подсчета эмоджи в строке
 export const countEmojis = (str: any, regex: RegExp): number => {
   return [...str.matchAll(regex)].length;
@@ -12,3 +16,9 @@ export const countEmojis = (str: any, regex: RegExp): number => {
 // export const countEmojis = (str: string, regex: RegExp): number => {
 //   return Array.from(str.matchAll(regex)).length;
 // };
+export const parseFormattedTextField = (field: string) => {
+  const rawContent = JSON.parse(field);
+  const contentState = convertFromRaw(rawContent);
+  const htmlContent = stateToHTML(contentState, FORMAT_OPTIONS);
+  return htmlContent;
+};
