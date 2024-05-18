2b9abe8c48062bc0e86785e0bf2ba57c50a0c193
Ihor Belodedov <i.belodedov@rqc.ru>
2024-05-18 16:44:13 +0300

    tg colors

--- src/assets/icons/StrikeThrough.tsx
+++ src/assets/icons/StrikeThrough.tsx
@@ -5,7 +5,7 @@ const StrikethroughSVG = () => {
       height="24px"
       viewBox="0 -960 960 960"
       width="24px"
-      fill="#000"
+      fill="var(--tg-theme-button-text-color)"
     >
       <path d="M80-400v-80h800v80H80Zm340-160v-120H200v-120h560v120H540v120H420Zm0 400v-160h120v160H420Z" />
     </svg>
